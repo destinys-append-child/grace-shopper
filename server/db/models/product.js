ce8048c438b6d0eab23892c11f555aa72a8bf500
Matthew Chen <mc.2green@gmail.com>
2019-11-05 12:41:44 -0500

    added testspec file, edited changes to pull request

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -4,6 +4,7 @@ const db = require('../db')
 const Product = db.define('product', {
   name: {
     type: Sequelize.STRING,
+    unique: true,
     allowNull: false,
     validate: {
       notEmpty: true
