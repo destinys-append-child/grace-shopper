01dac33fd4bd03f06639b43f29ab3d4bed09ebe7
Tayla Monet <taymjenk@gmail.com>
2019-11-07 17:08:29 -0500

    user login home page complete

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -7,7 +7,7 @@ import {logout} from '../store'
 import {Login, Signup} from '../components'
 
 import './navbar.css'
-import {Button} from 'semantic-ui-react'
+import {Button, Icon} from 'semantic-ui-react'
 
 class Navbar extends React.Component {
   constructor(props) {
@@ -40,39 +40,47 @@ class Navbar extends React.Component {
     return (
       <div id="navbar">
         <h1>YACHTS DON'T STOP</h1>
-        <h4>A One Stop Shop for Yachts</h4>
-        <nav>
-          {isLoggedIn ? (
-            <div>
-              {/* The navbar will show these links after you log in */}
-              <Link to="/home">Home</Link>
-              <Link to="/cart">Cart </Link>
-              <Link to="/wishlist">Wishlist </Link>
-              <a href="#" onClick={handleClick}>
-                Logout
-              </a>
-            </div>
-          ) : (
-            <div>
-              {/* The navbar will show these links before you log in */}
-              <Link to="/cart">Cart </Link>
-              <Link to="/wishlist">Wishlist </Link>
-              <Button
-                type="button"
-                onClick={this.viewLoginForm}
-                className="ui button active"
-              >
-                Login
-              </Button>
-              {this.state.viewLogin ? <Login /> : null}
-              <button type="button" onClick={this.viewSignupForm}>
-                Signup
-              </button>
-              {this.state.viewSignup ? <Signup /> : null}
-              {/*INPUT SEARCH BAR*/}
-            </div>
-          )}
-        </nav>
+        <div className="right-nav">
+          <h4>A One Stop Shop for Yachts</h4>
+          <nav>
+            {isLoggedIn ? (
+              <div>
+                {/* The navbar will show these links after you log in */}
+                <Link to="/home">
+                  <Icon name="home" color="white" />
+                </Link>
+                <Link to="/cart">
+                  <Icon name="cart" color="white" />
+                </Link>
+                <Link to="/wishlist">
+                  <Icon name="heart outline" color="white" />
+                </Link>
+                <a href="#" onClick={handleClick}>
+                  Logout
+                </a>
+              </div>
+            ) : (
+              <div>
+                {/* The navbar will show these links before you log in */}
+                <Link to="/cart">Cart </Link>
+                <Link to="/wishlist">Wishlist </Link>
+                <Button
+                  type="button"
+                  onClick={this.viewLoginForm}
+                  className="ui button active"
+                >
+                  Login
+                </Button>
+                {this.state.viewLogin ? <Login /> : null}
+                <button type="button" onClick={this.viewSignupForm}>
+                  Signup
+                </button>
+                {this.state.viewSignup ? <Signup /> : null}
+                {/*INPUT SEARCH BAR*/}
+              </div>
+            )}
+          </nav>
+        </div>
       </div>
     )
   }
