6c9613077016e38612599e93aa5fdfd471637164
Matthew Chen <mc.2green@gmail.com>
2019-11-09 19:25:08 -0500

    checkout routes, started checkout-page component

--- client/components/checkout.js
+++ client/components/checkout.js
@@ -1,19 +1,34 @@
 import React, {Component} from 'react'
-// import {connect} from 'react-redux'
+import {connect} from 'react-redux'
 import Cart from './cart'
+import {getTotalCostThunk} from '../store/checkout'
 
-export default class Checkout extends Component {
+class Checkout extends Component {
   constructor(props) {
     super(props)
     console.log(props)
   }
+  componentDidMount() {
+    this.props.getTotalCost()
+  }
 
   render() {
+    const {cost} = this.props
     return (
       <div>
         <h1>THIS IS THE CHECKOUT</h1>
         <Cart />
+        <h1>TOTAL COST:{cost.orderCost} </h1>
       </div>
     )
   }
 }
+const mapStateToProps = state => ({
+  cost: state.cost
+})
+
+const mapDispatchToProps = dispatch => ({
+  getTotalCost: () => dispatch(getTotalCostThunk())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Checkout)
