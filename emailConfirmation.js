689c5feb1c8d8c12ec48f02e6cbb4c4301cea808
Victor <Vguerrero42@gmail.com>
2019-11-12 11:03:46 -0500

    not yet

--- emailConfirmation.js
+++ emailConfirmation.js
@@ -9,7 +9,7 @@ const transporter = nodemailer.createTransport({
 })
 const sendEmail = async () => {
   let info = await transporter.sendMail({
-    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    from: process.env.NODEMAIL_EMAIL,
     to: 'bar@example.com, baz@example.com', // list of receivers
     subject: 'Hello ✔', // Subject line
     text: 'Hello world?', // plain text body
